188f54c69a8c52e21c78b48d5db06c141e93462f
LF-13 <l.fernandes0308@gmail.com>
2023-01-17 14:57:10 -0300

    Novo commit: Classes Utilitárias - DateFormat

--- logica/src/MaratonaJavaViradoNoJiraya/javacore/Rdatas/CalendarTest01Video113.java
+++ logica/src/MaratonaJavaViradoNoJiraya/javacore/Rdatas/CalendarTest01Video113.java
@@ -1,10 +1,25 @@
 package MaratonaJavaViradoNoJiraya.javacore.Rdatas;
 
 import java.util.Calendar;
+import java.util.Date;
 
 public class CalendarTest01Video113 {
     public static void main(String[] args) {
         Calendar c = Calendar.getInstance();
-        System.out.println(c);
+        if (c.getFirstDayOfWeek() == Calendar.SUNDAY) {
+            System.out.println("Domingo é o primeiro dia da semana");
+        }
+        System.out.println(c.get(Calendar.DAY_OF_WEEK));
+        System.out.println(c.get(Calendar.DAY_OF_MONTH));
+        System.out.println(c.get(Calendar.DAY_OF_YEAR));
+        System.out.println(c.get(Calendar.DAY_OF_WEEK_IN_MONTH));
+
+        c.add(Calendar.DAY_OF_MONTH, 0);
+        c.roll(Calendar.HOUR, 12);
+        Date date = c.getTime();
+        System.out.println(date);
     }
 }
+
+
+//COMMENT
